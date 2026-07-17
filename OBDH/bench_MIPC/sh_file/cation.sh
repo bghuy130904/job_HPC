a832bf70bc12d6b55ac88b67857c5c37540e5d0b
bghuy130904 <bghuy1309@gmail.com>
2026-07-17 08:14:54 +0700

    MIPC

--- OBDH/bench_MIPC/sh_file/cation.sh
+++ OBDH/bench_MIPC/sh_file/cation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=cation_4 ### Job name
-#SBATCH --output=/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/_output/cation_4.out          ### Standard output file
-#SBATCH --error=/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/_error/cation_4.err             ### Standard error file
+#SBATCH --job-name=cation_3 ### Job name
+#SBATCH --output=/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/_output/cation_3.out          ### Standard output file
+#SBATCH --error=/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/_error/cation_3.err             ### Standard error file
 #SBATCH --partition=Bigmem            ### queue
 #SBATCH --nodes=1                     ### Number of nodes
 #SBATCH --ntasks=1                    ### Number of tasks per node
@@ -13,9 +13,9 @@ start=$(date +%s)
 # input-file/code trong đường dẫn /home
 INPUT_FILE="/home/giahuy/Code/job/OBDH/bench_MIPC/_src/cation.py"
 # Ghi output trực tiếp ra /data
-OUTPUT_DIR="/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/$SLURM_JOB_ID"
+OUTPUT_DIR="/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/system_3"
 mkdir -p $OUTPUT_DIR
-OUTPUT_FILE="$OUTPUT_DIR/cation_4.txt"
+OUTPUT_FILE="$OUTPUT_DIR/cation_3.txt"
 
 # ====================================================================#
 # LƯU Ý: 2 DÒNG COMMAND NÀY LÀ BẮT BUỘC PHẢI CÓ TRONG FILE SUBMIT JOB
