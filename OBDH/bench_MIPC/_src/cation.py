9699443883897d8cb1fee2f1c6acc3788919a4f7
bghuy130904 <bghuy1309@gmail.com>
2026-07-29 09:02:45 +0700

    MIPC-OBMP2

--- OBDH/bench_MIPC/_src/cation.py
+++ OBDH/bench_MIPC/_src/cation.py
@@ -1,6 +1,6 @@
 import time
 from pyscf import scf, gto
-from pycmf.OBDH import OBMP2_CL
+from pycmf.OBDH import OBDH_CL
 
 mol = gto.Mole()
 mol.atom = '''
@@ -94,7 +94,7 @@ mf_emb = scf.UHF(mol).density_fit()
 mf_emb.max_memory = 120000            
 mf_emb.with_df.max_memory = 120000    
 mf_emb.run()
-mppp_emb = OBMP2_CL(mf_emb)
+mppp_emb = OBDH_CL(mf_emb)
 mppp_emb.alphaa = (0.53, 0.39)
 mppp_emb.thresh = 1e-08
 mppp_emb.second_order = True
--- OBDH/bench_MIPC/sh_file/cation.sh
+++ OBDH/bench_MIPC/sh_file/cation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=cation_5 ### Job name
-#SBATCH --output=/data/giahuy/Result/OBMP2_in_DFT/Ligands_Protein/_output/cation_5.out          ### Standard output file
-#SBATCH --error=/data/giahuy/Result/OBMP2_in_DFT/Ligands_Protein/_error/cation_5.err             ### Standard error file
+#SBATCH --output=/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/_output/cation_5.out          ### Standard output file
+#SBATCH --error=/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/_error/cation_5.err             ### Standard error file
 #SBATCH --partition=Bigmem            ### queue
 #SBATCH --nodes=1                     ### Number of nodes
 #SBATCH --ntasks=1                    ### Number of tasks per node
@@ -11,9 +11,9 @@
 
 start=$(date +%s)
 # input-file/code trong đường dẫn /home
-INPUT_FILE="/home/giahuy/Code/job/OBMP2/bench_MIPC/_src/cation.py"
+INPUT_FILE="/home/giahuy/Code/job/OBDH/bench_MIPC/_src/cation.py"
 # Ghi output trực tiếp ra /data
-OUTPUT_DIR="/data/giahuy/Result/OBMP2_in_DFT/Ligands_Protein/system_5"
+OUTPUT_DIR="/data/giahuy/Result/OBDH_in_DFT/Ligands_Protein/system_5"
 mkdir -p $OUTPUT_DIR
 OUTPUT_FILE="$OUTPUT_DIR/cation_5.txt"
 
