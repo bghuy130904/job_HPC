4a7f0a18373a7468d9a98d6fc4275d0f7c793838
bghuy130904 <bghuy1309@gmail.com>
2026-08-12 10:17:23 +0700

    retinal

---
+++ OBMP2/bench_retinal/sh_file/retinal.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#SBATCH --job-name=retinal_0 ### Job name
+#SBATCH --output=/data/giahuy/Result/OBMP2_in_DFT/retinal/_output/retinal_0.out          ### Standard output file
+#SBATCH --error=/data/giahuy/Result/OBMP2_in_DFT/retinal/_error/retinal_0.err             ### Standard error file
+#SBATCH --partition=Bigmem            ### queue
+#SBATCH --nodes=1                     ### Number of nodes
+#SBATCH --ntasks=1                    ### Number of tasks per node
+#SBATCH --cpus-per-task=16            ### Number of CPU cores per task
+#SBATCH --threads-per-core=1
+#SBATCH --mem=150G
+
+start=$(date +%s)
+# input-file/code trong đường dẫn /home
+INPUT_FILE="/home/giahuy/Code/job/OBMP2/bench_retinal/_src/obdh.py"
+# Ghi output trực tiếp ra /data
+OUTPUT_DIR="/data/giahuy/Result/OBMP2_in_DFT/retinal"
+mkdir -p $OUTPUT_DIR
+OUTPUT_FILE="$OUTPUT_DIR/retinal_0.txt"
+
+# ====================================================================#
+# LƯU Ý: 2 DÒNG COMMAND NÀY LÀ BẮT BUỘC PHẢI CÓ TRONG FILE SUBMIT JOB
+# ====================================================================#
+export JOB_SCRATCH_PATH="/scratch/$SLURM_JOB_ID"
+export TMPDIR="$JOB_SCRATCH_PATH"
+# ====================================================================#
+
+#Load các Module như bình thường
+
+module load python3.9
+source /home/giahuy/.venv/bin/activate
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+PYTHONPATH= python $INPUT_FILE > $OUTPUT_FILE
+
+echo "Job hoàn tất."
+echo "Output đã được ghi trực tiếp vào: $OUTPUT_FILE"
+
+#Ket thuc dem gio
+end=$(date +%s)
+
+runtime=$((end - start))
+
+hours=$((runtime / 3600))
+minutes=$(((runtime % 3600) / 60))
+seconds=$((runtime % 60))
+
+printf "Thời gian: %02d:%02d:%02d (giờ:phút:giây)\n" $hours $minutes $seconds
