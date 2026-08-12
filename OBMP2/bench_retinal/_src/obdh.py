6eacd3913b62e81e20a0627b9d6a5a3822a3f0e8
bghuy130904 <bghuy1309@gmail.com>
2026-08-12 10:23:33 +0700

    retinal

--- OBMP2/bench_retinal/_src/obdh.py
+++ OBMP2/bench_retinal/_src/obdh.py
@@ -52,13 +52,13 @@ mol.charge = 2
 mol.spin = 0
 mol.verbose = 4
 mol.basis = 'def2-tzvpd'
-mol.max_memory = 150000
+mol.max_memory = 130000
 mol.build()
 
 print("\n\n>>>>>>>> CHẠY CHẾ ĐỘ: CÓ EMBEDDING & TRUNCATION <<<<<<<<")
 mf_emb = scf.UHF(mol).density_fit()
-mf_emb.max_memory = 150000            
-mf_emb.with_df.max_memory = 150000    
+mf_emb.max_memory = 130000            
+mf_emb.with_df.max_memory = 130000    
 mf_emb.run()
 mppp_emb = OBMP2_CL(mf_emb)
 mppp_emb.alphaa = (0.53, 0.39)
