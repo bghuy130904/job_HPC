191f2e7e7c8df990f17330978fb30b411dd73275
bghuy130904 <bghuy1309@gmail.com>
2026-08-17 22:12:42 +0700

    obdh

--- OBDH/bench_dipole_152/sh_file/merge_sp.sh
+++ OBDH/bench_dipole_152/sh_file/merge_sp.sh
@@ -9,7 +9,7 @@ CODE="/home/giahuy/Code/job/OBDH/bench_dipole_152/_src/calc_dipmomt_obdh.py"
 for tag in sp nsp; do
     [ -d "$BASE/$tag/rows" ] || continue
     python "$CODE" --outdir "$BASE/$tag" --merge \
-           --input  \
+           --input "/home/giahuy/Code/job/CCSD/bench_dipole_152/geometry/${tag}_inputs.json" \
            --output-xlsx "$BASE/$tag/dipole_obdh_${tag}.xlsx" \
-           --ref-json "/home/giahuy/Code/job/CCSD/geometry/ref_ccsdt.json"
+           --ref-json "/home/giahuy/Code/job/CCSD/bench_dipole_152/geometry/ref_sp.json"
 done
